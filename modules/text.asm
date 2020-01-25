7bf2ceee7b316248f23833895763243c117d6420
zxwanderer <@zxwanderer>
2020-01-25 20:01:24 +0300

    show text screen is worked

--- modules/text.asm
+++ modules/text.asm
@@ -9,19 +9,19 @@ MODULE text
 
 print_screen_me:
     PUSH HL
-    ; MemSetTextBank
-    ; CALL input.noKey
-    ; CALL screen.clear; очищаем экран
-    ; POP HL
+    CALL input.noKey
+    CALL screen.clear; очищаем экран
+    POP HL
     mLDA; загрузили цвет
-    ; PUSH HL
-    ; CALL screen.set_colors
-    ; POP HL
+    PUSH HL
+    CALL screen.set_colors
+    POP HL
     mLDE
-    ; PUSH HL
-    ; LD HL, SCREEN_ADDR
-    ; EX HL, DE
-    ; CALL Text68.print_68at
+    PUSH HL
+    LD HL, SCREEN_ADDR
+    EX HL, DE
+    MemSetTextBank
+    CALL Text68.print_68at
     POP HL
     JP zxengine.process
 
