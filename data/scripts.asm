ae2dad2c2157a5077ce8cceb72aa549903a99b25
zxwanderer <@zxwanderer>
2020-01-26 21:02:39 +0300

    black border

--- data/scripts.asm
+++ data/scripts.asm
@@ -1,8 +1,8 @@
 BEGIN_SCRIPT:
+  CallCode black_border
   printScreen PAPER_BLACK, PEN_GREEN, HELLO_TXT
   CallCode copy_map
 LOOP_SCRIPT:
-  CallCode random_border
   CallCode show_map
   GoTo LOOP_SCRIPT
   defb _endByte
