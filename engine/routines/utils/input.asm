9b24c43f171068ec62f2361409031a70bb54f659
zxwanderer <@zxwanderer>
2020-02-21 15:57:01 +0300

    some update

--- engine/routines/utils/input.asm
+++ engine/routines/utils/input.asm
@@ -16,4 +16,21 @@ noKey:
 	jr nz,noKey
 	ret
 
+	; честно стырено из движка Wanderers by SamStyle
+scanKeys:
+	ld a,(HL) ;//  загружаем первый байт
+	and a  ;проверяем на 0
+	ret z ; возвращаем если 0
+	inc hl ; увеличиваем HL
+	in a,(0xfe) ; читаем значение
+	and (hl) ; сравниваем со вторым байтом
+	inc hl   ; увеличиваем указатель
+	ld e,(hl)
+	inc hl
+	ld d,(hl) ; запоминаем в DE указатель на процедуру
+	inc hl    ; увеличиваем HL
+	jr nz,scanKeys
+	or 2
+	ret
+
 ENDMODULE
